9bb9ccd3ba5e9bc86a0250be9a2fc6c170121808
mrbarge <matt.bargenquast@gmail.com>
2017-07-30 15:26:03 +1000

    updated layout and rest calls

--- app.py
+++ app.py
@@ -2,9 +2,8 @@
 import simplejson
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker, scoped_session
-from flask import Flask, request, session, g, redirect, url_for, abort, \
+from flask import Flask, request, flash, session, g, redirect, url_for, abort, \
      render_template, flash
-from pypickles.domain import customer, coffee, payment, base
 from pypickles.domain.customer import Customer
 from pypickles.domain.coffee import Coffee
 from pypickles.domain.payment import Payment
@@ -20,123 +19,128 @@ app.config.update(dict(
     DEBUG=False
 ))
 app.config.from_envvar('APP_CONFIG', silent=True)
+app.secret_key = app.config['SECRET_KEY']
 
-def connect_db():
+def get_db_engine():
     url = 'postgresql://{}:{}@{}:{}/{}'
     url = url.format(app.config['DB_USER'], app.config['DB_PASS'], app.config['DB_HOST'], app.config['DB_PORT'], app.config['DATABASE'])
     con = sqlalchemy.create_engine(url, client_encoding='utf8')
-    meta = sqlalchemy.MetaData(bind=con, reflect=True)
-    session_factory = sessionmaker(bind=con)
+    return con
+
+def get_db_session():
+    conn = get_db_engine()
+    if not hasattr(g, 'db_conn'):
+        g.db_conn = conn
+
+    meta = sqlalchemy.MetaData(bind=conn, reflect=True)
+    session_factory = sessionmaker(bind=conn)
     session = scoped_session(session_factory)
     return session, meta
 
-
 def init_db():
     """Initializes the database."""
-    session, meta = connect_db()
-    meta.drop_all()
-    customer_table = sqlalchemy.Table('customer', meta,
+    engine = get_db_engine()
+    conn = engine.connect()
+
+    metadata = sqlalchemy.MetaData()
+    customer_table = sqlalchemy.Table('customer', metadata,
         sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
         sqlalchemy.Column('balance', sqlalchemy.Numeric),
         sqlalchemy.Column('user_name', sqlalchemy.String)
     )
-    coffee_table = sqlalchemy.Table('coffee', meta,
+    coffee_table = sqlalchemy.Table('coffee', metadata,
         sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
         sqlalchemy.Column('date', sqlalchemy.DateTime),
         sqlalchemy.Column('price', sqlalchemy.Numeric),
         sqlalchemy.Column('user_id', sqlalchemy.Integer)
     )
-    payment_table = sqlalchemy.Table('payment', meta,
+    payment_table = sqlalchemy.Table('payment', metadata,
         sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
         sqlalchemy.Column('amount', sqlalchemy.Numeric),
         sqlalchemy.Column('date', sqlalchemy.DateTime),
         sqlalchemy.Column('user_id', sqlalchemy.Integer),
     )
+    metadata.create_all(engine, checkfirst=True)
 
-    meta.create_all()
-    c = Customer(user_name='user1',balance=0)
-    session.add(c)
-    session.commit()
+    try:
+        conn.execute(customer_table.insert(), id=1, balance=0, user_name='user1')
+    except sqlalchemy.exc.IntegrityError:
+        pass
 
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Creates the database tables."""
-    init_db()
-    print('Initialized the database.')
-
+    conn.close()
 
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
-    if hasattr(g, 'db_con'):
-        g.db_con.close()
-
+    if hasattr(g, 'db_session'):
+        g.db_session.close()
 
 def get_db():
     """Closes the database again at the end of the request."""
-    if not hasattr(g, 'db_con'):
-        g.db_con, g.db_meta = connect_db()
-    return g.db_con
+    if not hasattr(g, 'db_session'):
+        g.db_session, g.db_meta = get_db_session()
+    return g.db_session
 
+
+@app.before_request
+def before_request():
+    db_session = get_db()
+    g.user = None
+    c = db_session.query(Customer).filter(Customer.user_name=='user2').first()
+    if c is not None:
+        g.user = c
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-
 @app.route('/coffeedate/<user_id>')
 def get_coffee_days(user_id):
-    session = get_db()
-    c = customer.Customer.find_by_id(session, int(user_id))
+    db_session = get_db()
+    c = Customer.find_by_id(db_session, int(user_id))
     return simplejson.dumps(c.as_dict(), use_decimal=True)
 
 
 @app.route('/coffeedays/<user_id>')
 def get_reviews(user_id):
-    session = get_db()
-    c = customer.Customer.find_by_id(session, int(user_id))
+    db_session = get_db()
+    c = Customer.find_by_id(db_session, int(user_id))
     return simplejson.dumps(c.as_dict(), use_decimal=True)
 
 
 @app.route('/coffee', methods=['POST'])
 def add_coffee():
-    errors = []
-    results = {}
-    user_id = 1
     try:
-        session = get_db()
-        c = customer.Customer.find_by_id(session, int(user_id))
-        c = coffee.Coffee(user=c,price=1)
-        session.add(c)
-        session.commit()
+        if hasattr(g, 'user') and g.user is not None:
+            db_session = get_db()
+            db_session.add(g.user)
+            db_session.commit()
+            flash('Recorded a coffee.', 'Info')
+        else:
+            flash('You are not a registered user.', 'Error')
     except Exception as err:
-        errors.append(
-            "Unable to add a coffee: {0}".format(str(err))
-        )
-    return render_template('home.html', errors=errors, results=results)
+        flash('Unable to add a coffee: {0}'.format(str(err)), 'Error')
+
+    return render_template('home.html')
 
 @app.route('/payment', methods=['POST'])
 def add_payment():
-    errors = []
-    results = {}
     user_id = 1
     try:
-        session = get_db()
-        amount = Decimal(request.form['amount'])
-        c = customer.Customer.find_by_id(session, int(user_id))
-        p = payment.Payment(user=c, amount=amount)
-        session.add(p)
-        session.commit()
-        print "done"
+        if hasattr(g, 'user') and g.user is not None:
+            session = get_db()
+            amount = Decimal(request.form['amount'])
+            p = Payment(user=g.user, amount=amount)
+            session.add(p)
+            session.commit()
+            flash('Added payment of ${}'.format(amount), 'Info')
+        else:
+            flash('You are not a registered user.', 'Error')
     except Exception as err:
-        print "{0}".format(str(err))
-        errors.append(
-            "Please supply a payment amount as a positive number."
-        )
-    return render_template('home.html', errors=errors, results=results)
+        flash('Please supply a payment amount as a positive number.', 'Error')
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
